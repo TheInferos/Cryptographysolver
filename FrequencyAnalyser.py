e451f877b4afa68792d4da212d544306573c75db
Hex C <TheInferos@users.noreply.github.com>
2018-01-19 17:04:36 +0000

    Master files updated

--- FrequencyAnalyser.py
+++ FrequencyAnalyser.py
@@ -1,5 +1,5 @@
 def analyser(inpt):
-    key =  {"A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" : 0, "F" : 0, "G" : 0, "H" : 0, "I" : 0, "J" : 0, "K" : 0, "L" : 0, "M" : 0, "N" : 0, "O" : 0, "P" : 0, "Q" : 0, "R" : 0, "S" : 0, "T" : 0, "U" : 0, "V" : 0, "W" : 0, "X" : 0, "Y" : 0, "Z" : 0, }
+    key = {"A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" : 0, "F" : 0, "G" : 0, "H" : 0, "I" : 0, "J" : 0, "K" : 0, "L" : 0, "M" : 0, "N" : 0, "O" : 0, "P" : 0, "Q" : 0, "R" : 0, "S" : 0, "T" : 0, "U" : 0, "V" : 0, "W" : 0, "X" : 0, "Y" : 0, "Z" : 0, }
     for char in inpt:
         val = int(ord(char))
         if (val <= 122) and (val >= 97):
@@ -17,19 +17,20 @@ def parse():
 
 def frequencyOrder(freq):
     order = []
-    for i in range(26):
+    for loop in range(26):
         letter = "-1"
         previous = -1
-        for j in range(26):
-            tLetter = chr(65+i)
+        for char in range(26):
+            tLetter = chr(65+char)
             current = freq[tLetter]
             if current > previous:
                 previous = current
                 letter = tLetter
         order.append(letter)
-        print(letter)
         freq[letter] = -1
-    print (order)
+    return order
+
+
 
 f = open("CipherText/sub.txt").read()
 freq = analyser(f)
--- SubstitutionCipher.py
+++ SubstitutionCipher.py
@@ -1,26 +1,84 @@
 import FrequencyAnalyser
 
-def frequencyOrder(freq):
-    order = []
-    for i in range(26):
-        letter = "-1"
-        previous = -1
-        for j in range(26):
-            tLetter = chr(65+i)
-            current = freq[tLetter]
-            if current > previous:
-                previous = current
-                letter = tLetter
-        order.append(letter)
-        print(letter)
-        freq[letter] = -1
-    print (order)
 
-f = open("CipherText/sub.txt").read()
-freq = FrequencyAnalyser.analyser(f)
-frequencyOrder(freq)
+def subCipher(file):
+
+    freq = FrequencyAnalyser.analyser(file)
+    letterOcc = FrequencyAnalyser.frequencyOrder(freq)
+    commonLetters = leterFreq()
+    file = forceSwitch(file,letterOcc,commonLetters)
+    #file = commonSwitch(file,letterOcc,commonLetters)
+    askSwitch(file)
+    print(file)
+
+def commonSwitch(file, letterOcc, commonLetters):
+
+    for i in range (5):
+        print("the most common letters are")
+        print(commonLetters)
+        mostPop = letterOcc[i]
+        message = "the " + str((i+1)) + " most common letter is " + str(mostPop) + " which letter would you like to replace it with\n"
+        inp= raw_input(message)
+        file = replaceLetters(file,mostPop, inp)
+    return file
+
+def askSwitch(file):
+    carry = True
+    print("the message currently reads")
+    print(file)
+    message = ""
+    while carry:
+        change = raw_input("What letter would you like to change (case Sensitive)\n")
+        to = raw_input("What would you like to change it to?\n")
+        file = replaceLetters(file, change, to)
+        print("the message currently reads")
+        print(file)
+        message += change + " has been changed to " + to + "\n"
+        carryon = raw_input("Would you like to change another? y/n \n")
+        if carryon == "n":
+            carry= False
 
 
 
+def forceSwitch(file, letterOcc, commonLetters):
+    temptext = list(file)
+    for i in range(len(commonLetters)):
+        letter = letterOcc[i]
+        switch = commonLetters[i]
+        for char in range(len(file)):
+            if file[char] == letter:
+                temptext[char] = switch
+    message = ""
+    for i in temptext:
+        message += i
+    return message
 
 
+def leterFreq():
+    order = ["e","t", "a"]
+    return order
+
+
+def replaceLetters(text, letter, replacement):
+    temptext = list(text)
+    for char in range(len(text)):
+        if text[char] == letter:
+            temptext[char] = replacement
+    message = ""
+    for i in temptext:
+        message += i
+    print(message)
+    inp=raw_input("are you happy with the change? y/n\n")
+    if inp == "y":
+        return message
+    else:
+        return text
+
+
+def openFile():
+    file = open("CipherText/sub.txt","r").read()
+    return file
+
+file = openFile()
+print(file)
+subCipher(file)
